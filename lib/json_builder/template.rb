822cfa8133cd2c3c2f4fb4f4303f66681224b6d6
dewski <xhtmlthis@me.com>
2011-11-27 21:51:18 +0000

    Default pretty print environment

--- lib/json_builder/template.rb
+++ lib/json_builder/template.rb
@@ -1,7 +1,7 @@
 module ActionView #:nodoc:
   class Base
     cattr_accessor :pretty_print_json
-    @@pretty_print_json = true
+    @@pretty_print_json = defined?(Rails) && Rails.env.development?
     
     cattr_accessor :json_callback
     @@json_callback = true
